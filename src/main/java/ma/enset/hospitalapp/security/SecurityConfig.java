6d35e8d25994ee6036163925320a5bf8071f0fe5
OTMANE <othmandak87@gmail.com>
2025-06-28 13:25:55 +0100

    commit

--- src/main/java/ma/enset/hospitalapp/security/SecurityConfig.java
+++ src/main/java/ma/enset/hospitalapp/security/SecurityConfig.java
@@ -1,11 +1,18 @@
 package ma.enset.hospitalapp.security;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,6 +22,12 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Primary
     public InMemoryUserDetailsManager inMemoryUserDetailsManager(PasswordEncoder passwordEncoder){
         String encodedPassword = passwordEncoder.encode("1234");
         System.out.println(encodedPassword);
@@ -23,6 +36,16 @@ public class SecurityConfig {
             User.withUsername("user2").password(encodedPassword).roles("USER").build(),
             User.withUsername("admin").password(encodedPassword).roles("USER","ADMIN").build()
         );
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(@Qualifier("inMemoryUserDetailsManager") UserDetailsService userDetailsService, 
+                                                      PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+        
+        return new ProviderManager(authenticationProvider);
     }
 
     @Bean
